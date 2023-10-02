3daaf34acabdccb28ee81053eb6572ac8b1a8f98
Leo Silva <leocardosodev@gmail.com>
2023-10-02 17:55:04 -0300

    refactor: move mapping to MongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -4,10 +4,7 @@ export const MongoHelper = {
   client: null as MongoClient,
 
   async connect (uri: string) {
-    this.client = await MongoClient.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
+    this.client = await MongoClient.connect(process.env.MONGO_URL)
   },
 
   async disconnect () {
