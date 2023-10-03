6611dacd7cb98837efce06e5ee93d8cc29d75512
leoCardosoDev <leocardosodev@gmail.com>
2023-10-03 06:55:44 -0300

    refactor: connect to mongo db before integration test

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import setupMiddlewares from './middlewares'
+import setupRoutes from './routes'
 
 const setupApp = express()
 setupMiddlewares(setupApp)
+setupRoutes(setupApp)
 export default setupApp
