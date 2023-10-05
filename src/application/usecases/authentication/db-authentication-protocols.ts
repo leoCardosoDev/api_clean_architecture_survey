ea8495db26ebb30ba857569be7b5cd6ece914aaf
leoCardosoDev <leocardosodev@gmail.com>
2023-10-05 07:00:56 -0300

    refactor: rename TokerGenerator to Encrypter

--- src/application/usecases/authentication/db-authentication-protocols.ts
+++ src/application/usecases/authentication/db-authentication-protocols.ts
@@ -1,6 +1,6 @@
 export * from '../../../domain/model/account'
 export * from '../../../domain/usecase/authentication'
 export * from '../../protocols/criptogrphy/hash-comparer'
-export * from '../../protocols/criptogrphy/token-generator'
+export * from '../../protocols/criptogrphy/encrypter'
 export * from '../../protocols/db/load-account-by-email-repository'
 export * from '../../protocols/db/update-access-token-repository'
