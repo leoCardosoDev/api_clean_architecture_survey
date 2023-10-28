fbb274c0a7cd189d7611e6709f9f770689d80bc8
leoCardosoDev <leocardosodev@gmail.com>
2023-10-28 08:16:27 -0300

    chore: adjust mongo db

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb+srv://leodevnew:<password>@cluster0.1latbma.mongodb.net/',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || 'TfUf*89&'
 }
