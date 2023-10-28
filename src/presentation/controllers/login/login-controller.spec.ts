9f9f3df07b8f1cbc1ead7aa02a48ad28b34a6422
leoCardosoDev <leocardosodev@gmail.com>
2023-10-28 19:05:14 -0300

    refactor: remove code error 2

--- src/presentation/controllers/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login-controller.spec.ts
@@ -21,7 +21,7 @@ const makeAuthentication = (): Authentication => {
 
 const makeValidation = (): Validation => {
   class ValidationStub implements Validation {
-    validate (input: any): Error {
+    validate (input: any): any {
       return null
     }
   }
@@ -58,7 +58,7 @@ describe('Login Controller', () => {
 
   test('Should returns 401 if invalid credentials are provided', async () => {
     const { sut, authenticationStub } = makeSut()
-    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+    jest.spyOn(authenticationStub, 'auth').mockReturnValueOnce(new Promise(resolve => resolve('')))
     const httpResponse = await sut.handle(makeFakeRequest())
     expect(httpResponse).toEqual(unauthorized())
   })
