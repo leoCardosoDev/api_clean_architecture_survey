eb2df1eb2449e8cbe61c12d6ff15a14eca91a888
leoCardosoDev <leocardosodev@gmail.com>
2023-10-30 16:19:20 -0300

    test: ensure AccountMongoRepository find an account by token without role

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -60,6 +60,24 @@ describe('Account Mongo Repository', () => {
     })
   })
 
+  describe('loadByToken()', () => {
+    test('Should return account on loadByToken success without role', async () => {
+      const sut = makeSut()
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+  })
+
   describe('updateAccessToken()', () => {
     test('Should update the account accessToken on updateAccesstoken success', async () => {
       const sut = makeSut()
