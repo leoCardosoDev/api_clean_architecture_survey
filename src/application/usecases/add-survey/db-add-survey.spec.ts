14cd3b17fcdf98af1be8512ed471e4b4a0bd13c5
leoCardosoDev <leocardosodev@gmail.com>
2023-10-31 14:44:37 -0300

    test: ensure LoadSurveysController calls LoadSurveyUsecase

--- src/application/usecases/add-survey/db-add-survey.spec.ts
+++ src/application/usecases/add-survey/db-add-survey.spec.ts
@@ -39,7 +39,7 @@ describe('DbAddSurvey Usecase', () => {
     MockDate.set(new Date())
   })
 
-  beforeAll(() => {
+  afterAll(() => {
     MockDate.reset()
   })
   test('Should call AddSurveyRepository with correct values', async () => {
