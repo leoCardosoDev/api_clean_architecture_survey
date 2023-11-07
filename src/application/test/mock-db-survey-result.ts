25898f62fa469c779c6b7760366a28e29bb7d69c
leoCardosoDev <leocardosodev@gmail.com>
2023-11-07 05:35:41 -0300

    refactor: move mockLoadSurveyResultRepository to a separate file

--- src/application/test/mock-db-survey-result.ts
+++ src/application/test/mock-db-survey-result.ts
@@ -2,6 +2,7 @@ import { SurveyResultModel } from '@/domain/model/survey-result'
 import { SaveSurveyResultParams } from '@/domain/usecase/survey-result/save-survey-result'
 import { mockSaveSurveyResultModel } from '@/domain/test'
 import { SaveSurveyResultRepository } from '@/application/protocols/db/survey-result/save-survey-result-repository'
+import { LoadSurveyResultRepository } from '@/application/protocols/db/survey-result/load-survey-result-repository'
 
 export const mockSaveSurveyResultRepository = (): SaveSurveyResultRepository => {
   class SaveSurveyResultRepositoryStub implements SaveSurveyResultRepository {
@@ -10,4 +11,13 @@ export const mockSaveSurveyResultRepository = (): SaveSurveyResultRepository => 
     }
   }
   return new SaveSurveyResultRepositoryStub()
+}
+
+export const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
+  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
+    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
+      return Promise.resolve(mockSaveSurveyResultModel())
+    }
+  }
+  return new LoadSurveyResultRepositoryStub()
 }
--- src/application/usecases/load-survey-result/db-load-survey-result.spec.ts
+++ src/application/usecases/load-survey-result/db-load-survey-result.spec.ts
@@ -1,16 +1,6 @@
-import { mockSaveSurveyResultModel } from '@/domain/test'
+import { mockLoadSurveyResultRepository } from '@/application/test'
 import { LoadSurveyResultRepository } from '@/application/protocols/db/survey-result/load-survey-result-repository'
-import { SurveyResultModel } from '../survey-result/save-survey-result/db-save-survey-result-protocols'
 import { DbLoadSurveyResult } from './db-load-survey-result'
-
-const mockLoadSurveyResultRepository = (): LoadSurveyResultRepository => {
-  class LoadSurveyResultRepositoryStub implements LoadSurveyResultRepository {
-    async loadBySurveyId (surveyId: string): Promise<SurveyResultModel> {
-      return Promise.resolve(mockSaveSurveyResultModel())
-    }
-  }
-  return new LoadSurveyResultRepositoryStub()
-}
 
 type SutTypes = {
   sut: DbLoadSurveyResult
