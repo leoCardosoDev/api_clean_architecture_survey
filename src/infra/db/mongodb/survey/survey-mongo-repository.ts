2f1bec51499706c1aff1f12aa15d559cd2afa72b
leoCardosoDev <leocardosodev@gmail.com>
2023-11-09 05:27:09 -0300

    feat: ensure SurveyMongoRepository return loadAll surveys with didAnswer true or false

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -3,7 +3,7 @@ import { AddSurveyParams } from '@/domain/usecase/survey/add-survey'
 import { AddSurveyRepository } from '@/application/protocols/db/survey/add-survey-repository'
 import { LoadSurveysRepository } from '@/application/protocols/db/survey/load-surveys-repository'
 import { LoadSurveyByIdRepository } from '@/application/usecases/survey/load-survey-by-id/db-load-survey-by-id-protocols'
-import { MongoHelper } from '../helpers/mongo-helper'
+import { MongoHelper, QueryBuilder } from '../helpers/'
 import { ObjectId } from 'mongodb'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository, LoadSurveyByIdRepository {
@@ -12,9 +12,33 @@ export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRe
     await surveyCollection.insertOne(data)
   }
 
-  async loadAll (): Promise<SurveyModel[]> {
+  async loadAll (accountId: string): Promise<SurveyModel[]> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
-    const surveys = await surveyCollection.find().toArray()
+    const query = new QueryBuilder().lookup({
+      from: 'surveyResults',
+      foreignField: 'surveyId',
+      localField: '_id',
+      as: 'result'
+    }).project({
+      _id: 1,
+      question: 1,
+      answers: 1,
+      date: 1,
+      didAnswer: {
+        $gte: [{
+          $size: {
+            $filter: {
+              input: '$result',
+              as: 'item',
+              cond: {
+                $eq: ['$$item.accountId', new ObjectId(accountId)]
+              }
+            }
+          }
+        }, 1]
+      }
+    }).build()
+    const surveys = await surveyCollection.aggregate(query).toArray()
     return MongoHelper.mapCollection(surveys)
   }
 
